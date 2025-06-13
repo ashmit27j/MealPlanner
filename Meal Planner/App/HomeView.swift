b6993cb652013ad19e1e3d8c48c18cfa8e8f21fc
Ashmit <ashmit27j@gmail.com>
2025-06-13 16:53:50 +0530

    Add: File structure. UIKit and SwiftUI integration incomplete, repo has errors, bugfix soon

---
+++ Meal Planner/App/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Meal Planner
+//
+//  Created by MacBook on 13/06/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
